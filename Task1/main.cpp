ad33e53efc1cd79002757c09bb037a681ec2e68a
Rizza771 <rissni2025@mail.ru>
2024-06-11 23:36:46 +0900

    inital commit

---
+++ Task1/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include "trianglecalc.h"
+
+// раскомментировать строку ниже, чтобы отключить assert()
+// #define NDEBUG
+#include <cassert> // для assert
+
+#define _USE_MATH_DEFINES
+
+int main(){
+
+	// Проверка функции расчета длины отрезка
+	assert(triangleSideLength(0.0, 0.0, 0.0, 0.0) < FLT_EPSILON); // Проверка отрезка в нулевых координатах
+	assert((triangleSideLength(5.0, 5.0, -5.0, 5.0) - 10.0) < FLT_EPSILON); // Проверка отрезка в I - II четвертях
+	assert((triangleSideLength(7.0, 9.0, -11.0, -13.0) - 28.42534080710379) < FLT_EPSILON); // Проверка I - III четверти
+
+	// Проверка функции расчета периметра
+	assert((trianglePerimeter(10.0, 20.0, 30.0) - 60.0) < FLT_EPSILON); // Ровные значения
+	assert((trianglePerimeter(10.123456, 20.987987, 30.456654) - 61.568097) < FLT_EPSILON); // Значения с частью после запятой
+	assert((trianglePerimeter(-16.666665, -16.666665, 66.66666) - 33.33333) < FLT_EPSILON); // Отрицательные числа
+
+	// Проверка функции расчета площади
+	assert(triangleArea(1.0, 1.0, 0.0) < FLT_EPSILON); // Площадь равна 0 при любой стороне равной 0
+	assert(triangleArea(10.0, 20.0, 30.0) < FLT_EPSILON); // Если сумма двух сторон равна третьей, площадь равна 0
+	assert((triangleArea(42.0, 21.0, 56.0) - 374.577678859) < FLT_EPSILON); // Проверка на обычных значениях
+
+	setlocale(LC_ALL, "Russian"); // Для русского языка в консоли
+	
+	float ax, ay, bx, by, cx, cy; // Координаты вершин треугольника
+	
+	std::cout << "Введите координаты x, y точки A: ";
+	std::cin >> ax >> ay;
+	std::cout << "Введите координаты x, y точки B: ";
+	std::cin >> bx >> by;
+	std::cout << "Введите координаты x, y точки C: ";
+	std::cin >> cx >> cy;
+
+	std::cout << "Заданы вершины треугольника ABC: ";
+	std::cout << "A(" << ax << ", " << ay << "), ";
+	std::cout << "B(" << bx << ", " << by << "), ";
+	std::cout << "C(" << cx << ", " << cy << ")" << '\n';
+
+	// Вычисление длин сторон треугольника
+	float sideA = triangleSideLength(ax, ay, bx, by);
+	float sideB = triangleSideLength(bx, by, cx, cy);
+	float sideC = triangleSideLength(cx, cy, ax, ay);
+
+	std::cout << "Периметр треугольника ABC: " << trianglePerimeter(sideA, sideB, sideC) << '\n';
+	std::cout << "Площадь треугольника ABC: " << triangleArea(sideA, sideB, sideC) << '\n';
+	return 0;
+
+}
---
+++ Task2/main.cpp
@@ -0,0 +1,109 @@
+﻿#include "arrmethods.h"
+#include "checkerfunc.h"
+#include "vectmethods.h"
+#include <iostream>
+
+// раскомментировать строку ниже, чтобы отключить assert()
+// #define NDEBUG
+#include <cassert> // для assert
+
+void arrExec() {
+	
+	using namespace dynarr;
+
+	int n;
+	int min;
+	int max;
+
+	std::cout << "Введите натуральное число n: ";
+	std::cin >> n;
+	std::cout << "Введите нижний предел генерации случайных чисел: ";
+	std::cin >> min;
+	std::cout << "Введите верхний предел генерации случайных чисел: ";
+	std::cin >> max;
+
+	int* rand_arr = new int[n];
+	int* fact_arr = new int[n];
+
+	arrRandom(rand_arr, n, min, max);
+	std::cout << "Массив случайных чисел. ";
+	std::cout << "Элементы массива: ";
+	outputArray(rand_arr, n);
+
+	arrFactorial(fact_arr, n);
+	std::cout << "Массив факториалов. ";
+	std::cout << "Элементы массива: ";
+	outputArray(fact_arr, n);
+
+}
+
+void vectExec() {
+
+	using namespace vect;
+
+	int n;
+	int min;
+	int max;
+
+	std::cout << "Введите натуральное число n: ";
+	std::cin >> n;
+	std::cout << "Введите нижний предел генерации случайных чисел: ";
+	std::cin >> min;
+	std::cout << "Введите верхний предел генерации случайных чисел: ";
+	std::cin >> max;
+
+	std::vector<int> rand_vect(n);
+	std::vector<int> fact_vect(n);
+
+	arrRandom(rand_vect, min, max);
+	std::cout << "Массив случайных чисел. ";
+	std::cout << "Элементы массива: ";
+	outputArray(rand_vect);
+
+	arrFactorial(fact_vect);
+	std::cout << "Массив факториалов. ";
+	std::cout << "Элементы массива: ";
+	outputArray(fact_vect);
+
+}
+
+int main() {
+
+	{
+		std::vector<int> test_vect(5);
+		assert(checkVectRandom(test_vect));
+		assert(checkVectFactorial(test_vect));
+	}
+
+	{
+		size_t test_arr_size = 5;
+		int* test_arr = new int[test_arr_size];
+		assert(checkArrRandom(test_arr, test_arr_size));
+		assert(checkArrFactorial(test_arr, test_arr_size));
+	}
+
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "Выберите режим работы:" << '\n';
+	std::cout << "1. Динамические массивы." << '\n';
+	std::cout << "2. Векторы." << '\n';
+	std::cout << "0. Выход." << '\n';
+
+	int input;
+	std::cin >> input;
+	switch (input) {
+	case 1:
+		system("cls");
+		arrExec();
+		break;
+	case 2:
+		system("cls");
+		vectExec();
+		break;
+	case 0:
+		exit(1);
+	}
+
+	return 0;
+
+}
